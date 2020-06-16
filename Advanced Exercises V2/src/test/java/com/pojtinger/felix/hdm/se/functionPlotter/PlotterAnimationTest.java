59afd2c978386f6408cfa07fc41d62a8c24a3009
Felicitas Pojtinger <felicitas@pojtinger.com>
2020-06-16 15:52:04 +0200

    feat: Add animation test

---
+++ Advanced Exercises V2/src/test/java/com/pojtinger/felix/hdm/se/functionPlotter/PlotterAnimationTest.java
@@ -0,0 +1,30 @@
+package com.pojtinger.felicitas.hdm.se.functionPlotter;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class PlotterAnimationTest {
+    private Plotter plotter;
+
+    @BeforeEach
+    private void setUp() {
+        this.plotter = new PlotterImpl();
+    }
+
+    @Test
+    void animateSineWave() throws InterruptedException {
+        this.plotter.setYResolution(25);
+        this.plotter.setXResolution(100);
+
+        for (var i = 0; i < 1000; i++) {
+            final var offset = i;
+
+            System.out.println(this.plotter.getGraph((x) -> 10 * Math.sin(x + offset / 10)));
+
+            Thread.sleep(50);
+
+            System.out.print("\033[H\033[2J");
+            System.out.flush();
+        }
+    }
+}
