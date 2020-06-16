cbec3746cd932a283e897f2b54f482341feb49e8
Felicitas Pojtinger <felicitas@pojtinger.com>
2020-06-16 16:30:40 +0200

    feat: Enable selection of function to print

--- Advanced Exercises V2/src/main/java/com/pojtinger/felix/hdm/se/functionPlotter/Application.java
+++ Advanced Exercises V2/src/main/java/com/pojtinger/felix/hdm/se/functionPlotter/Application.java
@@ -4,20 +4,50 @@ import java.io.IOException;
 
 public class Application {
     public static void main(String[] args) throws InterruptedException, IOException {
-        Plotter plotter = new PlotterImpl();
+        try {
+            Plotter plotter = new PlotterImpl();
 
-        plotter.setYResolution(25);
-        plotter.setXResolution(100);
+            final var type = args[0];
+            final var yResolution = Integer.valueOf(args[1]);
+            final var xResolution = Integer.valueOf(args[2]);
 
-        for (var i = 0; i < 1000; i++) {
-            final var offset = i;
+            plotter.setYResolution(yResolution);
+            plotter.setXResolution(xResolution);
 
-            var graph = plotter.getGraph((x) -> 10 * Math.sin(x + offset / 10));
+            for (var i = 0; i < 1000; i++) {
+                final var offset = i;
 
-            System.out.print("\033[H\033[2J");
-            System.out.println(graph);
+                var graph = plotter.getGraph((x) -> {
+                    switch (type) {
+                        case "cos":
+                            return 10 * Math.cos(x + offset / 10);
 
-            Thread.sleep(50);
+                        case "sin":
+                            return 10 * Math.sin(x + offset / 10);
+
+                        case "exp":
+                            plotter.setXStart(-20);
+                            plotter.setXEnd(20);
+
+                            return Math.pow(x, offset / 10);
+
+                        case "log":
+                            return 10 * Math.log(x + offset / 10);
+
+                        default:
+                            throw new Error(String.format("Type %s not supported", type));
+                    }
+                });
+
+                System.out.print("\033[H\033[2J");
+                System.out.println(graph);
+
+                Thread.sleep(50);
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+
+            System.out.println("Usage: <sin|cos|exp|log> <y-resolution> <x-resolution>");
         }
     }
 }
